672c92b5c03b1625eec2a8a07241e5d9ca3246c4
Daniel Alves <daniel.alves@gazin.com.br>
2022-02-17 17:51:47 -0300

    Add Api calls

--- src/pages/Gallery/styles.ts
+++ src/pages/Gallery/styles.ts
@@ -3,3 +3,12 @@ import styled from "styled-components";
 export const Spacer = styled.div`
     min-height: 5rem;
 `;
+
+export const FullBlack = styled.div`
+    background-color: #181818;
+    min-width: 100%;
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
